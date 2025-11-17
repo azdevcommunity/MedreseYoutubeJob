854bb5563cf37eed0e70704bf8ec7dbfaa549fb8
yusifhasanov <yusif.hasanov17@gmail.com>
2025-11-17 16:29:07 +0400

    ligging

--- Program.cs
+++ Program.cs
@@ -8,6 +8,19 @@ using YoutubeApiSynchronize.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .Enrich.WithProperty("Application", "YoutubeApiSynchronize")
+        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName);
+});
+
+Log.Information("Application starting... Environment: {Environment}", 
+    builder.Environment.EnvironmentName);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
