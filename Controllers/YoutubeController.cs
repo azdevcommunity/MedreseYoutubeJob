53b9e67cb7a1688e985a293a31402dfaba8dee5d
yusifhasanov <yusif.hasanov17@gmail.com>
2025-11-17 16:56:55 +0400

    commit

--- Controllers/YoutubeController.cs
+++ Controllers/YoutubeController.cs
@@ -240,4 +240,61 @@ public class YoutubeController
 
         return Ok("Sucess");
     }
+
+    [HttpPost("UnsubscribeAndUpdateCallbackUrl")]
+    public async Task<IActionResult> UnsubscribeAndUpdateCallbackUrl()
+    {
+        string channelId = "UCN22jHS7MPBp38ZWZemt7i";
+        string newCallbackUrl = "https://api-ytb.nizamiyyemedresesi.az/api/Youtube/push";
+        string oldCallbackUrl = "https://api-ytb.nizamiyyemedresesi.az/api/youtube-pubsub/push";
+        string hubUrl = "https://pubsubhubbub.appspot.com/subscribe";
+        string topicUrl = $"https://www.youtube.com/feeds/videos.xml?channel_id={channelId}";
+
+        // Unsubscribe işlemi (Eski callback URL ile)
+        var unsubscribeContent = new FormUrlEncodedContent(new[]
+        {
+            new KeyValuePair<string, string>("hub.callback", oldCallbackUrl),
+            new KeyValuePair<string, string>("hub.mode", "unsubscribe"),
+            new KeyValuePair<string, string>("hub.topic", topicUrl),
+        });
+
+        using (var client = new HttpClient())
+        {
+            var unsubscribeResponse = await client.PostAsync(hubUrl, unsubscribeContent);
+            if (unsubscribeResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Unsubscribed successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to unsubscribe. Status code: {unsubscribeResponse.StatusCode}");
+                return BadRequest("Unsubscribe failed.");
+            }
+        }
+
+        // Subscribe işlemi (Yeni callback URL ile)
+        var subscribeContent = new FormUrlEncodedContent(new[]
+        {
+            new KeyValuePair<string, string>("hub.callback", newCallbackUrl),
+            new KeyValuePair<string, string>("hub.mode", "subscribe"),
+            new KeyValuePair<string, string>("hub.topic", topicUrl),
+            new KeyValuePair<string, string>("hub.verify", "sync")
+        });
+
+        using (var client = new HttpClient())
+        {
+            var subscribeResponse = await client.PostAsync(hubUrl, subscribeContent);
+            if (subscribeResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Successfully subscribed with new callback URL.");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to subscribe. Status code: {subscribeResponse.StatusCode}");
+                return BadRequest("Subscribe failed.");
+            }
+        }
+
+        return Ok("Subscription updated successfully.");
+    }
 }
